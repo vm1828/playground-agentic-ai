21efcd02a850c71003b6118d481358e9fb1cfcf1
vm1828 <vm18281828@gmail.com>
2025-09-30 19:40:03 +0200

    Add basic ReAct agents examples

---
+++ init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "" && echo "===================================================== PROJECT INIT" && echo ""
+
+if [ ! -d ".venv" ]; then
+    python3 -m venv .venv
+    source .venv/bin/activate
+    pip install wheel==0.45.1
+    pip install torch==2.8.0 --index-url https://download.pytorch.org/whl/cpu
+    pip install -r requirements.txt
+else
+    echo "Project already initialized."
+fi
+
+echo ""
