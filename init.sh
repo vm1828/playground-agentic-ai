8e4c109f2a3b0baf558284b7db5b891c861f3462
vm1828 <vm18281828@gmail.com>
2025-10-10 16:55:24 +0200

    Update deps

--- init.sh
+++ init.sh
@@ -3,6 +3,9 @@
 echo "" && echo "===================================================== PROJECT INIT" && echo ""
 
 if [ ! -d ".venv" ]; then
+    sudo apt update
+    sudo apt install graphviz libgraphviz-dev pkg-config
+    
     python3 -m venv .venv
     source .venv/bin/activate
     pip install wheel==0.45.1
